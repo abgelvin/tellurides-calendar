7cd320eafff4443d45864ec5c987a4d73b230bd4
abgelvin <60162973+abgelvin@users.noreply.github.com>
2023-08-30 19:41:36 -0600

    added code for save_to_db

--- app.py
+++ app.py
@@ -65,8 +65,56 @@ def save_to_db(events):
     db = os.getenv('DB_DB')
     )
 
-cu = db.cursor()
+    cu = db.cursor()
 
+    # Create reservations table
+    cu.execute('CREATE TABLE IF NOT EXISTS reservations (id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, date TEXT NOT NULL, start_time TEXT NOT NULL, flight TEXT NOT NULL, ride_type TEXT NOT NULL, party TEXT NOT NULL, origin TEXT NOT NULL, destination TEXT NOT NULL, etx TEXT NOT NULL)')
+
+    # Clear database table of previous query data
+    cu.execute('DELETE FROM reservations')
+
+    # For each event from get_event(), retrieve the start date and summary
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        summary = event['summary']
+
+        # Split the summary string at '-' to get the separate values and create list "fields"
+        fields = summary.split('-')
+
+        # If the first field of the summary is "cancelled," skip that event
+        if fields[0] =='CANCELLED':
+            continue
+        
+        try:
+            # If there are at least 6 fields and there is a start time, retrieve fields from event and insert into db table
+            if len(fields) > 5 and start:
+                # Split datetime into date and time
+                date_time = start.split('T')
+                date = date_time[0]
+                print(date)
+                if len(date_time) < 2:
+                    print(f'no date/time entered: {fields}')
+                    continue
+                start_time = date_time[1].split('-')[0]
+                start_time = start_time[:len(start_time)-3]
+
+                flight = fields[0]
+                ride_type = fields[1]
+                party = fields[2]
+                origin = fields[3]
+                destination = fields[4]
+                etx = fields[5]
+
+                # Add events to reservations database
+                cu.execute('INSERT INTO reservations (date, start_time, flight, ride_type, party, origin, destination, etx) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
+                           (date, start_time, flight, ride_type, party, origin, destination, etx,))
+
+                db.commit()
+
+        except Exception as e:
+            print('An error occurred: %s' % fields)
+
+            raise e
 
 
 
